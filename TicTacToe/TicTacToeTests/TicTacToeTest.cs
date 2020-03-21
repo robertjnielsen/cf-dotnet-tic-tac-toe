8d914b157dae8b3230b9af467387a48647cbcb24
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-21 16:11:36 -0700

    Testing for player input value to match position on board passes.

--- TicTacToe/TicTacToeTests/TicTacToeTest.cs
+++ TicTacToe/TicTacToeTests/TicTacToeTest.cs
@@ -8,7 +8,7 @@ namespace TicTacToeTests
     public class TicTacToeTest
     {
         [Fact]
-        public void CreatingPlayerReturnsTheNameOfTheNewPlayer()
+        public void CreatingPlayerObjectCanReturnTheNameOfTheNewPlayer()
         {
             string playerNameInput = "Dummmy";
 
@@ -29,6 +29,62 @@ namespace TicTacToeTests
             Player nextPlayer = testGame.NextPlayer();
 
             Assert.Equal(playerTwo, nextPlayer);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(9)]
+        public void PlayerPositionMatchesCorrectBoardRowIndex(int userInput)
+        {
+            int boardRowValue;
+            switch (userInput)
+            {
+                case 1:
+                    boardRowValue = 0;
+                    break;
+                case 5:
+                    boardRowValue = 1;
+                    break;
+                case 9:
+                    boardRowValue = 2;
+                    break;
+                default:
+                    boardRowValue = 0;
+                    break;
+            }
+
+            Position testPosition = Player.PositionForNumber(userInput);
+
+            Assert.Equal(boardRowValue, testPosition.Row);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(9)]
+        public void PlayerPositionMatchesCorrectBoardColumnIndex(int userInput)
+        {
+            int boardRowValue;
+            switch (userInput)
+            {
+                case 1:
+                    boardRowValue = 0;
+                    break;
+                case 5:
+                    boardRowValue = 1;
+                    break;
+                case 9:
+                    boardRowValue = 2;
+                    break;
+                default:
+                    boardRowValue = 0;
+                    break;
+            }
+
+            Position testPosition = Player.PositionForNumber(userInput);
+
+            Assert.Equal(boardRowValue, testPosition.Column);
         }
     }
 }
