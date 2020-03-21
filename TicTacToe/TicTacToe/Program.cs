f2d432825471f6b39a8ddb0e500624904a950605
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-21 13:49:58 -0700

    Creating Player One creates new Player instance, test passes to return instance and Name prop for new Player.

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -3,14 +3,29 @@ using TicTacToe.Classes;
 
 namespace TicTacToe
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's play Tic-Tac-Toe!");
 
             // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+
             // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner.
+        }
+
+        public static void NewGame()
+        {
+            Console.WriteLine("Please enter a name for Player One:");
+            string playerOneName = Console.ReadLine();
+            Player PlayerOne = NewPlayer(playerOneName);
+        }
+
+        public static Player NewPlayer(string playerName)
+        {
+            Player newPlayer = new Player();
+            newPlayer.Name = playerName;
+            return newPlayer;
         }
     }
 }
--- TicTacToe/TicTacToeTests/TicTacToeTest.cs
+++ TicTacToe/TicTacToeTests/TicTacToeTest.cs
@@ -1,15 +1,20 @@
 using System;
 using Xunit;
 using TicTacToe;
+using TicTacToe.Classes;
 
 namespace TicTacToeTests
 {
     public class TicTacToeTest
     {
         [Fact]
-        public void TestThatGameBoardOutputsToTheConsole()
+        public void CreatingPlayerOneReturnsTheNameOfPlayerOne()
         {
-            
+            string playerOne = "Dummmy";
+
+            Player playerOneResult = Program.NewPlayer(playerOne);
+
+            Assert.Equal(playerOneResult.Name, playerOne);
         }
     }
 }
