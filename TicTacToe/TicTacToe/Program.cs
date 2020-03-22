99e28486e06d1e98a73629084b037a803e1052f6
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-21 17:03:43 -0700

    Removed unnecessary methods to create new Player instances and a new Game instance.

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -5,10 +5,28 @@ namespace TicTacToe
 {
     public class Program
     {
+        /// <summary>
+        /// The main entry point for the application. Creates two new Player instances, a new Game instance, and outputs the results of the game to the console.
+        /// </summary>
+        /// <param name="args">Application arguments that may be passed at runtime.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Let's play Tic-Tac-Toe!");
-            Game game = NewGame();
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a name for Player One:");
+            string playerOneName = Console.ReadLine();
+            Player playerOne = new Player();
+            playerOne.Name = playerOneName;
+            playerOne.Marker = "X";
+
+            Console.WriteLine("Please enter a name for Player Two:");
+            string playerTwoName = Console.ReadLine();
+            Player playerTwo = new Player();
+            playerTwo.Name = playerTwoName;
+            playerTwo.Marker = "O";
+
+            Game game = new Game(playerOne, playerTwo);
 
             Player winner = game.Play();
 
@@ -22,41 +40,6 @@ namespace TicTacToe
             {
                 Console.WriteLine("It's a draw! Nobody wins...");
             }
-
-            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner.
-        }
-
-        /// <summary>
-        /// Creates a new Game instance, and assigns two new Player instances to the object.
-        /// </summary>
-        /// <returns>Returns a new Game object.</returns>
-        public static Game NewGame()
-        {
-            Console.WriteLine("Please enter a name for Player One:");
-            string playerOneName = Console.ReadLine();
-            Player playerOne = NewPlayer(playerOneName);
-            playerOne.Marker = "X";
-
-            Console.WriteLine("Please enter a name for Player Two:");
-            string playerTwoName = Console.ReadLine();
-            Player playerTwo = NewPlayer(playerTwoName);
-            playerTwo.Marker = "O";
-
-            Game newGame = new Game(playerOne, playerTwo);
-
-            return newGame;
-        }
-
-        /// <summary>
-        /// Creates a new Player instance, and assigns the Name property of the Player object to the user's input.
-        /// </summary>
-        /// <param name="playerName">User input to be assigned as the Name property of the new Player object.</param>
-        /// <returns>Returns the new Player object, with a user determined Name property.</returns>
-        public static Player NewPlayer(string playerName)
-        {
-            Player newPlayer = new Player();
-            newPlayer.Name = playerName;
-            return newPlayer;
         }
     }
 }
